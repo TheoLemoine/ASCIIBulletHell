e938942a2cc097e3d88f47dfb609f585291ed13c
Daxter <daxter5onyx@gmail.com>
2020-10-07 11:20:34 +0200

    new dll

--- Inputs.h
+++ Inputs.h
@@ -2,17 +2,23 @@
 
 class ASCIIRenderer;
 
+#include <Windows.h>
+#include <Xinput.h>
+#pragma comment (lib, "xinput.lib")
 
 class Inputs
 {
 private:
 	bool IsPress(int vkey);
-
+	bool GetGamepadState();
+	XINPUT_STATE state;
+	int controllerId;
 public:
-	bool UpPress();
-	bool DownPress();
-	bool LeftPress();
-	bool RightPress();
+	Inputs();
+	float UpPress();
+	float DownPress();
+	float LeftPress();
+	float RightPress();
 	bool SpacePress();
 	bool EchapPress();
 	void DebugInputs(ASCIIRenderer* renderer, int x, int y);
